82ecd862aa5106c92e347e5c5e4db0f84492ca7c
Jared Smith <jared.craig.smith@gmail.com>
2018-10-02 15:50:04 -0600

    Started files

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿
+
+
+function GetRandomQuestion() {
+    var QuestionArray = ["Question", "Option1", "Option2", "Option3", "Option4", "Answer"];
+    //TODO: Fill question array with real data
+    return QuestionArray;
+}
+
+function CheckAnswer(QuestionArray, Answer) {
+    if (QuestionArray[5] == Answer) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
