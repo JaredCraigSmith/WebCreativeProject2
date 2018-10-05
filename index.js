e166df2df3bc8984a361721fda3e6c29d01b74a0
Jared Smith <jared.craig.smith@gmail.com>
2018-10-04 21:22:28 -0600

    final stuff

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ var LastQuestion;
 GetRandomQuestion();
 
 function GetRandomQuestion() {
-    var QuestionArray = ["Question", "Option1", "Option2", "Option3", "Option4", "Answer"];
+    var QuestionArray = [];
     var URL = "https://opentdb.com/api.php?amount=1&type=multiple";
     $.getJSON(URL, function(data) {
-        QuestionArray[0] = data.results[0].question;
-        QuestionArray[1] = data.results[0].incorrect_answers[0];
-        QuestionArray[2] = data.results[0].incorrect_answers[1];
-        QuestionArray[3] = data.results[0].incorrect_answers[2];
-        QuestionArray[4] = data.results[0].correct_answer;
+        
+        QuestionArray[0] = data.results[0].incorrect_answers[0];
+        QuestionArray[1] = data.results[0].incorrect_answers[1];
+        QuestionArray[2] = data.results[0].incorrect_answers[2];
+        QuestionArray[3] = data.results[0].correct_answer;
+
+        QuestionArray = shuffle(QuestionArray);
+
+        QuestionArray[4] = data.results[0].question;
         QuestionArray[5] = data.results[0].correct_answer;
+
+        for (i = 0; i < QuestionArray.length; i++) {
+            QuestionArray[i] = QuestionArray[i].replace(/&quot;/g, "\"")
+            QuestionArray[i] = QuestionArray[i].replace(/&#039;/g, "\'")
+            QuestionArray[i] = QuestionArray[i].replace(/&euml/g, "ë")
+        }
+        
 
         CurrentQuestion = QuestionArray;
         return QuestionArray;
@@ -30,12 +41,12 @@ function GetAndDisplayRandomQuestion() {
     $("#Option3").css("background-color","white")
     $("#Option4").css("background-color","white")
 
-    $("#Question").text(CurrentQuestion[0]);
-    $("#Option1").text(CurrentQuestion[1])
-    $("#Option2").text(CurrentQuestion[2]);
-    $("#Option3").text(CurrentQuestion[3])
+    $("#Question").text(CurrentQuestion[4]);
+    $("#Option1").text(CurrentQuestion[0])
+    $("#Option2").text(CurrentQuestion[1]);
+    $("#Option3").text(CurrentQuestion[2])
+    $("#Option4").text(CurrentQuestion[3])
 
-    $("#Option4").text(CurrentQuestion[4])
     LastQuestion = CurrentQuestion
     GetRandomQuestion();
 
@@ -63,4 +74,23 @@ function DisplayCheckAnswer(Optionid){
          $("#"+Optionid).css("background-color","red")
     }
     
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
